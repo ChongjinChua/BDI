ef89a2d7c311cdc49ba1ab9d9043dda7ef9a35b3
ChongJin <chua1@purdue.edu>
2016-06-13 00:10:10 -0400

    played around with beautifulsoup. Haven't figured out how to parse html from morningstar.com . will look into json

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
 BDI project
 
-- Scrapes company data from webpages and then perform fundamental analysis. Result is then dumped to an Excel sheet.
+- Scrapes company data from webpages and then perform a fundamental analysis. Results are then dumped onto an Excel sheet.
 - Goal is to assist investors in making better decision when investing in stock market
---
+++ playground/bs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import requests
+
+print("hi")
+r = requests.get('http://www.morningstar.com/stocks/xnas/aapl/quote.html',headers={'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0'})
+
+html = r.text
+page = BeautifulSoup(html,'html.parser')
+#data = page.find('tr',{'class':'gr_table_row4'})
+print(page)
