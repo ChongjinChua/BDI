c8401e3b282266697a81adfe3817e67c123ee8d6
ChongJin <chua1@purdue.edu>
2016-06-20 23:55:58 -0400

    done preparing urls, next step is to process urls into html_pages

---
+++ src/BDI_mainProgram/Prep_input_file.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# http://financials.morningstar.com/income-statement/is.html?t=SYMBOL&region=USA&culture=en_US | earnings, revenue
+# http://financials.morningstar.com/balance-sheet/bs.html?t=SYMBOL&region=USA&culture=en_US    | long-term debt, cash & equivalent, short-term debt
+# http://financials.morningstar.com/cash-flow/cf.html?t=SYMBOL&region=USA&culture=en_US        | cash flow
+# http://financials.morningstar.com/ratios/r.html?t=SYMBOL&region=USA&culture=en_US            | gross margin, net margin, return on equity
+# http://www.reuters.com/finance/stocks/financialHighlights?symbol=SYMBOL                      | Long Term growth rate
+# http://www.reuters.com/finance/stocks/overview?symbol=SYMBOL                                 | Beta, outstanding shares
+
+import pandas
+
+csvfile = "../../input_files/constituents-financials.csv"
+MS_is_head = "http://financials.morningstar.com/income-statement/is.html?t="
+MS_bs_head = "http://financials.morningstar.com/balance-sheet/bs.html?t="
+MS_cf_head = "http://financials.morningstar.com/cash-flow/cf.html?t="
+MS_r_head = "http://financials.morningstar.com/ratios/r.html?t="
+MS_tail = "&region=USA&culture=en_US"
+R_fh_head = "http://www.reuters.com/finance/stocks/financialHighlights?symbol="
+R_o_head = "http://www.reuters.com/finance/stocks/overview?symbol="
+
+data = pandas.read_csv(csvfile)
+symbols = data.Symbol.tolist()
+
+url_list = []
+for symbol in symbols[0:2]:
+    MS_url1 = MS_is_head + symbol + MS_tail
+    MS_url2 = MS_bs_head + symbol + MS_tail
+    MS_url3 = MS_cf_head + symbol + MS_tail
+    MS_url4 = MS_r_head + symbol + MS_tail
+    R_url1 = R_fh_head + symbol
+    R_url2 = R_o_head + symbol
+    url_list.append(tuple([MS_url1,MS_url2,MS_url3,MS_url4,R_url1,R_url2]))
+
+print(url_list)
+
